0232b18e050a826a605e8c3ea7e07d599c1c8546
Nicholas Cabral <nicholasscabral@gmail.com>
2023-09-24 11:50:42 -0300

    feat: add Payments webhook constants

--- src/client/AsaasClient.ts
+++ src/client/AsaasClient.ts
@@ -1,25 +1,31 @@
-import axios from 'axios';
-import { AsaasOptions } from '../types/AsaasTypes';
-import { CustomersAPI } from "./Customers"
-import { PaymentsAPI } from './Payments';
-import { InstallmentsAPI } from './Installments';
-import { SubscriptionsAPI } from './Subscriptions';
+import axios from 'axios'
+import { AsaasOptions } from '../types/AsaasTypes'
+import { CustomersAPI } from './Customers'
+import { PaymentsAPI } from './Payments'
+import { InstallmentsAPI } from './Installments'
+import { SubscriptionsAPI } from './Subscriptions'
+import { Webhooks } from './Webhooks'
 
 export class AsaasClient {
-  public customers: CustomersAPI;
-  public payments: PaymentsAPI;
-  public installments: InstallmentsAPI;
-  public subscriptions: SubscriptionsAPI;
-  
-  constructor(private apiKey: string, options: AsaasOptions = {}) {
-    const apiClient = axios;
-    apiClient.defaults.baseURL = options.sandbox ? (options.sandboxUrl || 'https://sandbox.asaas.com/api/v3') : options.baseUrl ? options.baseUrl : 'https://api.asaas.com/v3';
-    apiClient.defaults.headers.common['access_token'] = this.apiKey;
+  public customers: CustomersAPI
+  public payments: PaymentsAPI
+  public installments: InstallmentsAPI
+  public subscriptions: SubscriptionsAPI
+  public webhooks: Webhooks
 
-    this.customers = new CustomersAPI(apiClient);
-    this.payments = new PaymentsAPI(apiClient);
-    this.installments = new InstallmentsAPI(apiClient);
-    this.subscriptions = new SubscriptionsAPI(apiClient);
+  constructor(private apiKey: string, options: AsaasOptions = {}) {
+    const apiClient = axios
+    apiClient.defaults.baseURL = options.sandbox
+      ? options.sandboxUrl || 'https://sandbox.asaas.com/api/v3'
+      : options.baseUrl
+      ? options.baseUrl
+      : 'https://api.asaas.com/v3'
+    apiClient.defaults.headers.common['access_token'] = this.apiKey
+
+    this.customers = new CustomersAPI(apiClient)
+    this.payments = new PaymentsAPI(apiClient)
+    this.installments = new InstallmentsAPI(apiClient)
+    this.subscriptions = new SubscriptionsAPI(apiClient)
+    this.webhooks = new Webhooks()
   }
 }
-
